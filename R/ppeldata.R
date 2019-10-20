54d4b86ffc6984c639e803958ca43b5ef409a446
Vlad Tarko <vladtarko@gmail.com>
2019-10-19 20:28:46 -0700

    added VDEM

--- R/ppeldata.R
+++ R/ppeldata.R
@@ -18,3 +18,38 @@
 #' data(qog)
 #'
 "qog"
+
+
+#' Varieties of Democracy full dataset
+#'
+#' The full cross-section panel data: Country-Year: V-Dem Full+Others
+#' All 450+ V-Dem indicators and indices + 59 other indicators from other data sources.
+#'
+#' @docType data
+#'
+#' @usage data(vdem)
+#'
+#' @keywords datasets
+#'
+#' @references
+#' Coppedge, Michael, John Gerring, Carl Henrik Knutsen, Staffan I. Lindberg, Jan Teorell,
+#' David Altman, Michael Bernhard, M. Steven Fish, Adam Glynn, Allen Hicken, Anna Lührmann,
+#' Kyle L. Marquardt, Kelly McMann, Pamela Paxton, Daniel Pemstein, Brigitte Seim, Rachel Sigman,
+#' Svend-Erik Skaaning, Jeffrey Staton, Steven Wilson, Agnes Cornell, Lisa Gastaldi, Haakon Gjerløw,
+#' Nina Ilchenko, Joshua Krusell, Laura Maxwell, Valeriya Mechkova, Juraj Medzihorsky, Josefine Pernes,
+#' Johannes von Römer, Natalia Stepanova, Aksel Sundström, Eitan Tzelgov, Yi-ting Wang, Tore Wig,
+#' and Daniel Ziblatt. 2019. "V-Dem [Country-Year/Country-Date] Dataset v9",
+#' _Varieties of Democracy (V-Dem) Project_. \href{https://doi.org/10.23696/vdemcy19}
+#'
+#' Pemstein, Daniel, Kyle L. Marquardt, Eitan Tzelgov, Yi-ting Wang, Juraj Medzihorsky, Joshua Krusell,
+#' Farhad Miri, and Johannes von Römer. 2019. “The V-Dem Measurement Model: Latent Variable Analysis
+#' for Cross-National and Cross-Temporal Expert-Coded Data”, V-Dem Working Paper No. 21. 4th edition.
+#' University of Gothenburg: Varieties of Democracy Institute.
+#'
+#' @source \href{https://www.v-dem.net/en/data/data-version-9/}{Varieties of Democracy}
+#'
+#' @examples
+#' data(vdem)
+#'
+"vdem"
+
