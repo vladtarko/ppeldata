02eac7d0f0e827cc749d16e094a51b2f97c51243
Vlad Tarko <vladtarko@gmail.com>
2019-10-19 20:17:16 -0700

    initial setup

---
+++ R/ppeldata.R
@@ -0,0 +1,20 @@
+#' Quality of Government dataset
+#'
+#' The full cross-section standard panel data.
+#'
+#' @docType data
+#'
+#' @usage data(qog)
+#'
+#' @keywords datasets
+#'
+#' @references Teorell, Jan et al. 2018. The Quality of Government Standard Dataset,
+#'    Version Jan18. University of Gothenburg: The Quality of Government Institute.
+#' (\href{https://qog.pol.gu.se/data/datadownloads/qogstandarddata}{Quality of Government Institute})
+#'
+#' @source \href{https://qog.pol.gu.se/data/datadownloads/qogstandarddata}{Quality of Government Institute}
+#'
+#' @examples
+#' data(qog)
+#'
+"qog"
